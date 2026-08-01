1ebd9dbc848a62637ac3d39ad19f91286da1f8b5
immiren <immi.renko@gmail.com>
2026-08-01 16:35:45 +0300

    Cleanup, remove unused variables

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,15 @@ int cellSize = 100;	// size in px
 const int cellCountX = 9;
 const int cellCountY = 9;
 
-int cells[cellCountX][cellCountY] = {
+struct Cell {
+	int value = 0; // 0 = empty
+	bool prefilled = false;
+	bool notes[9] = { false,false,false,false,false,false,false,false,false };
+};
+
+Cell cells[cellCountX][cellCountY];
+
+int cellNumbers[cellCountX][cellCountY] = {
 	{0,1,0,0,0,4,0,0,0},
 	{4,2,0,0,0,0,0,0,0},
 	{0,3,0,0,0,0,4,0,0},
@@ -20,15 +28,34 @@ int cells[cellCountX][cellCountY] = {
 	{0,0,0,0,4,0,0,0,0},
 	{0,0,0,0,0,0,0,4,0}
 };
-// TODO flip to correct x and y values
+// TODO flip to correct x and y values or just replace
 
 vector<int> cursorLocation = { 0,0 };
 
 bool gameOver = false;
 bool redraw = true;
+bool takingNotes = false;
+
+void fillCells() {
+	for (int x = 0; x < cellCountX; x++) {
+		for (int y = 0; y < cellCountY; y++) {
+			bool prefilled = false;
+			if (cellNumbers[x][y] != 0) prefilled = true;
+
+			Cell cell = {
+				cellNumbers[x][y],
+				prefilled,
+				{ false,false,false,false,false,false,false,false,false },
+			};
+			cells[x][y] = cell;
+		}
+	}
+}
 
 void drawHighlight(int posX, int posY) {
-	DrawRectangle(posX * cellSize, posY * cellSize, cellSize, cellSize, SKYBLUE);
+	Color c = SKYBLUE;
+	if (takingNotes) c = PURPLE;
+	DrawRectangle(posX * cellSize, posY * cellSize, cellSize, cellSize, c);
 }
 
 void drawCell(int posX, int posY) {
@@ -47,11 +74,38 @@ void drawCell(int posX, int posY) {
 	if (posY == 3 || posY == 6 || posY == 0) DrawRectangle(posX * cellSize, posY * cellSize, cellSize, lineSize, BLACK);
 
 	// draw numbers on cells with values
-	const int* cell = &cells[posX][posY];
-	if (*cell != 0) {
-		std::string text = std::to_string(*cell);
-		if (*cell == 1) DrawText(text.c_str(), posX * cellSize + 40, posY * cellSize + 10, 90, BLACK);
-		else DrawText(text.c_str(), posX * cellSize + 30, posY * cellSize + 10, 90, BLACK);
+	const int* cellValue = &cells[posX][posY].value;
+	if (*cellValue != 0) {
+		Color c = BLACK;
+		if (!cells[posX][posY].prefilled) c = DARKPURPLE;
+		std::string text = std::to_string(*cellValue);
+		if (*cellValue == 1) DrawText(text.c_str(), posX * cellSize + 40, posY * cellSize + 10, 90, c);
+		else DrawText(text.c_str(), posX * cellSize + 30, posY * cellSize + 10, 90, c);
+	}
+	else {
+		// draw notes
+		for (int n = 0; n < cellCountX; n++) {
+			if (cells[posX][posY].notes[n] == true) {
+				// draw note
+				int noteX;
+				int noteY;
+
+				// get placement
+				if (n >= 0 && n < 3) noteY = 10; // top row
+				else if (n >= 3 && n < 6) noteY = 40; // middle row
+				else noteY = 70; // bottom row
+				if (n == 0 ) noteX = 15; // offset 1 to the right
+				else if (n == 3 || n == 6) noteX = 10; // left column
+				else if (n == 1 || n == 4 || n == 7) noteX = 40; // middle column
+				else noteX = 70; // right column
+
+				// draw number
+				std::string text = std::to_string(n + 1);
+				DrawText(text.c_str(), posX * cellSize + noteX, posY * cellSize + noteY, 30, GRAY);
+			}
+
+
+		}
 	}
 }
 
@@ -71,22 +125,14 @@ int main(void) {
 	SetTargetFPS(60);
 	int windowWidth = cellSize * cellCountX;
 	int windowHeight = cellSize * cellCountY ;
-	InitWindow(windowWidth, windowHeight, "test window");
-
-	int size = 100;
-	int posX = 30;
-	int posY = (windowHeight-size)/2;
-
-	Color c;
-	c.r = 100;
-	c.g = 170;
-	c.b = 255;
-	c.a = 255;
+	InitWindow(windowWidth, windowHeight, "Sudoku");
 
 	Vector2 direction = { 1, 0 };
 
 	const float moveTimeDuration = 0.1;
 	float timer = moveTimeDuration;
+	
+	fillCells();
 
 	while (!WindowShouldClose()) {
 		BeginDrawing();
@@ -100,6 +146,12 @@ int main(void) {
 			if (IsKeyDown(KEY_RIGHT)) { direction = { 1,0 }; }
 			if (direction.x != 0 || direction.y != 0) redraw = true;
 
+			// switch between writing and notetaking
+			if (IsKeyPressed(KEY_P)) {
+				takingNotes = !takingNotes;
+				redraw = true;
+			}
+
 			// number input: toimii mut paranna
 			int input = 0;
 			if (IsKeyPressed(KEY_ONE))  input = 1;
@@ -112,7 +164,11 @@ int main(void) {
 			if (IsKeyPressed(KEY_EIGHT))  input = 8;
 			if (IsKeyPressed(KEY_NINE))  input = 9;
 			if (input != 0) {
-				cells[cursorLocation[0]][cursorLocation[1]] = input;
+				// switch modes: pen/
+				if (cells[cursorLocation[0]][cursorLocation[1]].prefilled == false) {
+					if (takingNotes) cells[cursorLocation[0]][cursorLocation[1]].notes[input - 1] = !cells[cursorLocation[0]][cursorLocation[1]].notes[input - 1];
+					else cells[cursorLocation[0]][cursorLocation[1]].value = input;
+				}
 				redraw = true;
 			}
 
@@ -147,13 +203,12 @@ int main(void) {
 // TODO
 // - numerot
 //	- placeholder DONE
-//	 - fiksaa x ja y
-//  - oikeet
+//  - oikeitten generointi
 // - grid DONE
 // - grid lines DONE
-// - numerotyypit (eri väriset)
-//	- valmiit -> ei voi muuttaa
-//  - uudet -> voi muuttaa
+// - numerotyypit (eri väriset) DONE
+//	- valmiit -> ei voi muuttaa  DONE
+//  - uudet -> voi muuttaa  DONE
 // - input numero DONE
 //	- testaa onko nro ok
 // - random numerot
